f3394b612aeae30793b5a575b5fdcdd4b804c249
Kennedy Karimi on Mac <kennedymugera@gmail.com>
2024-12-24 15:41:54 +0300

    Initial Commit

---
+++ TabViewExplorer/TabViewExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewExplorerApp.swift
+//  TabViewExplorer
+//
+//  Created by Kennedy Karimi on 24/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct TabViewExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TabViewExplorerTests/TabViewExplorerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewExplorerTests.swift
+//  TabViewExplorerTests
+//
+//  Created by Kennedy Karimi on 24/12/2024.
+//
+
+import Testing
+@testable import TabViewExplorer
+
+struct TabViewExplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
